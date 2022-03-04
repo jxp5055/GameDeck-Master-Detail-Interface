87c4cfb4daacfeb2cf36f5b9b0f5ddf13edf475b
jxp5055 <janeypham5055@gmail.com>
2022-03-03 20:48:35 -0500

    3 binaries uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { GamesComponent } from './games/games.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, GamesComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/mock-games.ts
@@ -0,0 +1,14 @@
+import { Game } from './game';
+
+export const GAMES: Game[] = [
+  { id: 1, name: 'The Last of Us Part II' },
+  { id: 2, name: 'Ghost of Tsushima' },
+  { id: 3, name: 'Elden Ring' },
+  { id: 4, name: 'God of War' },
+  { id: 5, name: 'Life is Strange: True Colors' },
+  { id: 6, name: 'Cyberpunk' },
+  { id: 7, name: 'Uncharted' },
+  { id: 8, name: 'Resident Evil 2' },
+  { id: 9, name: 'Final Fantasy 7: Remake' },
+  { id: 10, name: 'Animal Crossing: New Horizons' },
+];
